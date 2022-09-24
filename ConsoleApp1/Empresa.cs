a79b64bef5d6c784ee84f593c7b50a2891b84007
Roger Gonçalves <rogergoncalves_santos@hotmail.com>
2022-09-23 22:27:47 -0300

    aplicação c#

---
+++ ConsoleApp1/Empresa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Empresa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Empresa(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void IncrementaSalario(double porcetagem)
+        {
+            Salario += Salario * porcetagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleApp1;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
+using System.Xml;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionarios deseja cadastrar? ");
+            int n = int.Parse(Console.ReadLine());
+            List<Empresa> list = new List<Empresa>();
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Funcionario #" + i);
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine());
+                list.Add(new Empresa(id, nome, salario));
+            }
+            Console.WriteLine();
+            Console.Write("Entre com o id do funcionario que deseja dar aumento: ");
+            int procurarFuncionario = int.Parse(Console.ReadLine());
+            Empresa emp = list.Find(x => x.Id == procurarFuncionario);
+
+            if(emp != null)
+            {
+                Console.Write("Entre com o valor em porcetagem do aumento do funcionario: ");
+                double porcetagem = double.Parse(Console.ReadLine());
+                emp.IncrementaSalario(porcetagem);
+            }
+            else
+            {
+                Console.Write("Esse funcionario não existe!");
+            }
+            Console.WriteLine();
+            Console.Write("Funcionarios atualzados: ");
+            foreach(Empresa obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+} 
